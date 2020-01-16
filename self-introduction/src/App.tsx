55b6bf769e56b638cd1114542ab8a016bca728e5
김남현 <knh6210@gmail.com>
2020-01-16 10:23:44 +0900

    Main 화면 구성 완료

--- self-introduction/src/App.tsx
+++ self-introduction/src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import { Container } from '@material-ui/core';
+import {MainIntrodunction, introdunctionProps} from './component/introdunction/MainIntrodunction';
+
+const mainFeaturedPost :introdunctionProps = {
+  post : {
+    title: '나에대한 소개 ?',
+    description:
+      `성균관대학교, 
+      소프트웨어학과, 소프트웨어플랫폼학과,
+      Android, 이클립스 RCP, Theia , React , Typescript, Java 등등`,
+    image: 'https://source.unsplash.com/random',
+    imageText: 'main image description',
+    linkText: 'Continue reading…',
+  }
+};
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Container maxWidth="xl">
+        <Header 
+          title={'NamHyun Kim'}
+          buttonLable = {'Button'}
+        />
+        <MainIntrodunction post={mainFeaturedPost.post} />
+
+      </Container>
+      <Footer
+          title= {'Footer 임시'}
+          description = {'이곳에 설명쓰자'}
+        />
+    </React.Fragment>
   );
 }
 
---
+++ self-introduction/src/component/Header.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: 'space-between',
+    overflowX: 'auto',
+  },
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0,
+  },
+}));
+
+interface HedaerProps  {
+    title :string;
+    buttonLable : string;
+}
+
+export default function Header(props:HedaerProps) {
+  const classes = useStyles();
+  const { buttonLable, title } = props;
+
+  return (
+    <React.Fragment>
+      <Toolbar className={classes.toolbar}>
+        <Button size="small">{buttonLable}</Button>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          className={classes.toolbarTitle}
+        >
+          {title}
+        </Typography>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <Button variant="outlined" size="small">
+          Send Email
+        </Button>
+      </Toolbar>
+    </React.Fragment>
+  );
+}
