fe4fa011a4a612f1468b32037e6b8a9edb4b6cd5
김남현 <knh6210@gmail.com>
2020-01-30 13:11:45 +0900

    프로젝트부분 추가

--- self-introduction/src/component/introdunction/Project/GripSensing.tsx
+++ self-introduction/src/component/introdunction/Project/GripSensing.tsx
@@ -2,7 +2,8 @@ import React, { ReactNode } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import GridContainer from '../../Grid/GridContainer';
 import GridItem from '../../Grid/GridItem';
-
+import { Stepper, Step, StepLabel, StepContent, Typography, Button, Paper, Chip } from '@material-ui/core';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 
   
@@ -18,17 +19,148 @@ const useStyles = makeStyles({
         marginTop: "30px",
         minHeight: "32px",
         fontSize : "25px",
-      }
+    },
+    description : {
+        display: "inline-block",
+        position: "relative",
+        textDecoration: "none",
+    },
+    usedSkill : {
+        color: "#3C4858",
+        margin: "1.75rem 0 0.875rem",
+        textDecoration: "none",
+        fontWeight: 700,
+        fontFamily: `"Roboto Slab", "Times New Roman", serif`,
+        display: "inline-block",
+        position: "relative",
+        marginTop: "10px",
+        fontSize : "17",
+    },
+    steperContainer : {
+    
+    },
+    videoButton: {
+        marginLeft: "30px",
+        lineHeight: 1.5,
+    },
+    button: {
+        marginTop: "5px",
+        marginRight: "5px"
+    },
+    actionsContainer: {
+        marginBottom: "5px"
+    },
+    resetContainer: {
+        padding: "5px"
+    },
+    skillChip : {
+        margin : "5px"
+    },
 });
 
+function getSteps() {
+    return ['스마트폰에 분류할 그립 등록', '해당그립에 대한 실행 어플리케이션 설정', '해당그립과 Shaking을 통한 어플 실행'];
+}
+
+function getStepContent(step: number) {
+    switch (step) {
+      case 0:
+        return `For each ad campaign that you create, you can control how much
+                you're willing to spend on clicks and conversions, which networks
+                and geographical locations you want your ads to show on, and more.`;
+      case 1:
+        return 'An ad group contains one or more ads which target a shared set of keywords.';
+      case 2:
+        return `Try out different ad text to see what brings in the most customers,
+                and learn how to enhance your ads using features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`;
+      default:
+        return 'Unknown step';
+    }
+  }
 
 export default function GripSensing(props: any) {
     const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+    const handleNext = () => {
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+      };
+    
+      const handleBack = () => {
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+      };
+    
+      const handleReset = () => {
+        setActiveStep(0);
+      };
     return (
         <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={4}>
+            <GridItem xs={12} sm={12}>
                 <h3 className={classes.title}>GripSensing</h3>
+                <Button
+                        variant="contained"
+                        color="secondary"
+                        size="small"
+                        className={classes.videoButton}
+                        startIcon={<PlayArrowIcon />}
+                >
+                    Watch Video
+                </Button>
+   
+                <p>
+                    소리 신호를 이용한 스마트폰 그립감지 시스템{" "}
+                </p>
             </GridItem>
+            <GridItem xs={12} sm={12}>
+                <h3 className={classes.usedSkill}>사용 된 기술 { "  " }</h3>
+                <Chip className={classes.skillChip} variant="outlined" size="small" label="Android" color="primary" />
+                <Chip className={classes.skillChip}variant="outlined" size="small" label="SoundSignal Processing" color="primary" />
+                <Chip className={classes.skillChip} variant="outlined" size="small" label="machine learning" color="primary" />
+            </GridItem>
+            <GridItem xs={12} sm={12}>
+                <div className={classes.steperContainer}>
+                    <Stepper activeStep={activeStep} orientation="vertical">
+                        {steps.map((label, index) => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                            <StepContent>
+                            <Typography>{getStepContent(index)}</Typography>
+                            <div className={classes.actionsContainer}>
+                                <div>
+                                <Button
+                                    disabled={activeStep === 0}
+                                    onClick={handleBack}
+                                    className={classes.button}
+                                >
+                                    Back
+                                </Button>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={handleNext}
+                                    className={classes.button}
+                                >
+                                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                </Button>
+                                </div>
+                            </div>
+                            </StepContent>
+                        </Step>
+                        ))}
+                    </Stepper>
+                    {activeStep === steps.length && (
+                        <Paper square elevation={0} className={classes.resetContainer}>
+                        <Typography>All steps completed - you&apos;re finished</Typography>
+                        <Button onClick={handleReset} className={classes.button}>
+                            Reset
+                        </Button>
+                        </Paper>
+                    )}
+                </div>
+            </GridItem>
+            
         </GridContainer>
     );
 }
